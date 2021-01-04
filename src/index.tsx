9d6f688ba27809da28c90f2d80a56639c5edee30
shahzad6077 <alim6077@gmail.com>
2021-01-04 22:30:49 +0500

    enable register Pwa

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import { ThemeProvider } from "@material-ui/core";
 import theme from "./theme";
-import {
-  BrowserRouter as Router,
-} from 'react-router-dom';
-import { ApolloProvider } from "@apollo/client"
+import { BrowserRouter as Router } from "react-router-dom";
+import { ApolloProvider } from "@apollo/client";
 
 import gqlClient from "./Services/Graphql";
 
@@ -22,11 +20,10 @@ ReactDOM.render(
       </ApolloProvider>
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
-
+serviceWorkerRegistration.register();
